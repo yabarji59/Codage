180d17137f035f9f22db1f49bfa3d7fb50f4ec2a
wendy <wendy.vandenberghe@live.fr>
2021-10-07 14:56:39 +0200

    Update header.js

--- Wendy/Brief_HTML/exo1/js/header.js
+++ Wendy/Brief_HTML/exo1/js/header.js
@@ -1,7 +1,7 @@
 headerElement = `<header class="header">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">ShopWendy <img width = 50px src="./icon/vans.png"></img></a>
+        <a class="navbar-brand" href="shopwendy.html">ShopWendy <img width = 50px src="./icon/vans.png"></img></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,10 @@ headerElement = `<header class="header">
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.html">Accueil</a>
+                    <a class="nav-link active" aria-current="page" href="nike.html">Nike</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="vans.html">Vans</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link " href="contact.html">Contact</a>
