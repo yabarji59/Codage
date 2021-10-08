8c45dd926e52acdedbad3c34a00500b4db3c06f0
Virginie <virginie.finez@laposte.net>
2021-10-08 10:16:13 +0200

    Ajout de l'exercice afficher un formulaire

---
+++ Virginie/Brief_HTML/exercices JS/afficher un formulaire/script.js
@@ -0,0 +1,9 @@
+document.getElementById("formulaire").innerHTML = `
+<form>
+   <p>
+       <label for="message">Message</label>
+       <input type="text" name="message" id="message" />
+   </p>
+</form>
+`;
+document.getElementById('message').value="Vous avez réussi votre exercice !";
