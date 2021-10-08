5ff0dcedd3a50d89c137e3ccb595c8d7bd183fb4
karima <saidaniikarima@gmail.com>
2021-10-08 14:45:36 +0200

    feat_karima : test_api_JS

--- karimasaidani/Brief_HTML/js/script.js
+++ karimasaidani/Brief_HTML/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
     $('header').load('header.html');
     $('footer').load('footer.html')});
+
+  
+
+    function UserAction() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+             if (this.readyState == 4 && this.status == 200) {
+                 alert(this.responseText);
+             }
+        };
+
+        xhttp.open("GET", "ajax_info.txt", false);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send();
+        document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+    
