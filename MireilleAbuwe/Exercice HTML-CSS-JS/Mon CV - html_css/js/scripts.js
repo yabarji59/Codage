23a17375b8810e6bce5dd85bc08fef2ecc063f41
Charline8380 <charline.abuwe@gmail.com>
2021-10-12 09:49:03 +0200

    update my CV practice

---
+++ MireilleAbuwe/Exercice HTML-CSS-JS/Mon CV - html_css/js/scripts.js
@@ -0,0 +1,9 @@
+/*Personal Script*/
+$(function () {
+    $("#header").load("../header.html");
+    $("#footer").load("../footer.html");
+    $("#navmenu").load("../navmenu.html");
+    
+  });
+
+  /*Personal Script*/
